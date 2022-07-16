9223d2401632099fc93b351a66659ec88cfd2607
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-16 12:45:29 +0000

    [Add]user show

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  
+  def new
+    
+    
+    
+  end
+  
+  def index
+    
+    @books = Books.all
+    
+  end  
+  
+  def show
+    
+    
+    
+  end
+
+  def edit
+    
+    
+    
+  end
+  
+  def destroy
+    
+    
+    
+  end
+  
+  def update
+    
+    
+    
+  end
+  
+  private
+  
+  
+  
+end
