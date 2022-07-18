4371923d6404003804a93f97be37deca8d97c26c
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-18 10:51:50 +0000

    [add]bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
 
     @user = User.find(params[:id])
 
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
+
   end
 
   def update
