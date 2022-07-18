27df1c066ba8c467a6c5c646daa9fc894ece1102
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-18 06:46:28 +0000

    [Add]flash messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,13 @@ class BooksController < ApplicationController
 
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+
+    if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+    else
+     render books_path
+    end
 
   end
 
@@ -41,20 +46,24 @@ class BooksController < ApplicationController
   def update
 
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      render edit_book_path
+    end  
 
   end
 
   def destroy
-    
+
     @bookf = Book.find(params[:id])
     @bookf.destroy
     redirect_to books_path
-    
+
   end
-  
-  
+
+
   private
 
   def book_params
