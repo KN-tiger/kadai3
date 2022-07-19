b0d1b46059e390574bda77a3819e081c944bbd1d
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-19 16:03:33 +0000

    [Add]form layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,12 @@
 class BooksController < ApplicationController
 
   def new
-
     @book = Book.new
-
   end
 
   def create
-
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-
     if @book.save
      flash[:notice] = "You have created book successfully."
      redirect_to book_path(@book.id)
@@ -18,71 +14,51 @@ class BooksController < ApplicationController
       
       @books = Book.all
       render :index
-
     end
-
   end
 
   def index
     @book = Book.new
     @books = Book.all
     @user = current_user
-
   end
 
   def show
-
     @bookf = Book.find(params[:id])
     @user = @bookf.user
     @book = Book.new
-
-
-
   end
 
   def edit
-
     @book = Book.find(params[:id])
-    
-    if @book.user == current_user
+      if @book.user == current_user
       render "edit"
     else
       redirect_to books_path
     end
-
   end
 
   def update
-
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book)
     else
-      
       render :edit
-      
     end
-
   end
 
   def destroy
-
     @bookf = Book.find(params[:id])
     @bookf.destroy
     redirect_to books_path
-
   end
 
 
   private
 
   def book_params
-
     params.require(:book).permit(:title, :body)
-
   end
-
-
 
 end
