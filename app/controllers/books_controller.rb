5599eb8f39242a17cb91b55cf5480ece2c32acbc
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-07-20 05:41:29 +0000

    [Finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,14 +5,15 @@ class BooksController < ApplicationController
   end
 
   def create
+    
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
      flash[:notice] = "You have created book successfully."
      redirect_to book_path(@book.id)
     else
-      
       @books = Book.all
+      @user = current_user
       render :index
     end
   end
@@ -32,10 +33,10 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
       if @book.user == current_user
-      render "edit"
-    else
-      redirect_to books_path
-    end
+        render "edit"
+      else
+        redirect_to books_path
+      end
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,17 @@ class UsersController < ApplicationController
 
     @users = User.all
     @user = current_user
+    @book = Book.new
 
   end
 
 
   def show
-    
+
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
-    
+
   end
 
   def edit
